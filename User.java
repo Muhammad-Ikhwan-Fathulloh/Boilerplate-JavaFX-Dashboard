39f4507f636beb6c51ff5afb0f2d9af2093f0e38
Pandawan <145522644+ikhwan-fathulloh@users.noreply.github.com>
2025-01-18 01:48:02 +0700

    Update User.java

--- User.java
+++ User.java
@@ -4,11 +4,13 @@ public class User {
     private IntegerProperty id;
     private StringProperty username;
     private StringProperty password;
+    private StringProperty role;
 
-    public User(int id, String username, String password) {
+    public User(int id, String username, String password, String role) {
         this.id = new SimpleIntegerProperty(id);
         this.username = new SimpleStringProperty(username);
         this.password = new SimpleStringProperty(password);
+        this.role = new SimpleStringProperty(role);
     }
 
     // Getter and Property for 'id'
@@ -48,5 +50,17 @@ public class User {
 
     public void setPassword(String password) {
         this.password.set(password);
+    }
+    
+    public StringProperty roleProperty() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role.set(role);
+    }
+
+    public String getRole() {
+        return role.get();
     }
 }
